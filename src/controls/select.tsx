b9c087159f9295e0ad9ac57cd0cf68644b3315d6
Sagar Patil <sagarreddypatil@gmail.com>
2023-12-27 02:10:48 -0500

    description editing

---
+++ src/controls/select.tsx
@@ -0,0 +1,37 @@
+import { useId } from "react";
+import { twMerge } from "tailwind-merge";
+
+export function Select({
+  children,
+  onClick,
+  selected,
+  className,
+}: {
+  children: React.ReactNode;
+  onClick?: () => void;
+  selected?: boolean;
+  className?: string;
+}) {
+  const id = useId();
+
+  return (
+    <div className="flex">
+      <input
+        type="checkbox"
+        id={id}
+        className="peer hidden"
+        checked={selected}
+        onChange={() => onClick && onClick()}
+      />
+      <label
+        htmlFor={id}
+        className={twMerge(
+          "flex flex-row flex-1 items-center justify-center align-middle rounded-none outline outline-1 outline-black peer-checked:bg-black peer-checked:text-white text-normal px-4 text-lg shadow-[3px_3px_0px_1px_rgba(0,0,0,0.5)]",
+          className ? className : ""
+        )}
+      >
+        {children}
+      </label>
+    </div>
+  );
+}
--- src/pages/task.tsx
+++ src/pages/task.tsx
@@ -3,12 +3,13 @@ import useTasks from "../hooks/useTasks";
 import TaskCard from "../components/task-card";
 import { TaskStatus, defaultTaskColor } from "../types/task";
 import { HexColorPicker } from "react-colorful";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useDebounce } from "usehooks-ts";
 import { Button } from "../controls/button";
 import { pb } from "../Login";
 import Textbox from "../controls/textbox";
 import Markdown from "react-markdown";
+import { Select } from "../controls/select";
 
 // from https://github.com/Chalarangelo/30-seconds-of-code/blob/master/content/snippets/js/s/format-duration.md
 const formatDuration = (ms: number) => {
@@ -30,12 +31,28 @@ export default function Task() {
   const navigate = useNavigate();
 
   const { tasks, stateChanges, updateTask } = useTasks();
+  const [editDesc, setEditDesc] = useState(false);
+  const [newDesc, setNewDesc] = useState("");
   const { id } = useParams();
 
   const [color, setColor] = useState(defaultTaskColor);
   const debouncedColor = useDebounce(color, 500);
 
   const _task = tasks.find((task) => task.id === id);
+
+  useEffect(() => {
+    if (editDesc) {
+      setNewDesc(_task?.description ?? "");
+      return;
+    }
+
+    // update task description
+    if (!_task) return;
+
+    if (newDesc === undefined) return;
+
+    updateTask({ ..._task, description: newDesc });
+  }, [editDesc]);
 
   useEffect(() => {
     if (_task?.color) setColor(_task.color);
@@ -105,11 +122,38 @@ export default function Task() {
     <div className="flex flex-col gap-3">
       <TaskCard task={task} />
       <div className="h-2"></div>
-      <h2 className="text-2xl font-bold">Description</h2>
-      <div className="prose prose-black prose-hr:my-2 prose-headings:mt-6 prose-headings:mb-2 prose-p:my-0">
-        <Markdown>{task.description}</Markdown>
+      <div className="flex flex-row">
+        <h2 className="text-2xl font-bold flex-1">Description</h2>
+        <Select selected={editDesc} onClick={() => setEditDesc((val) => !val)}>
+          Edit Description
+        </Select>
       </div>
+      <hr className="border-gray-500" />
+      {editDesc ? (
+        <textarea
+          className="p-2 border border-black rounded-none text-lg"
+          defaultValue={task.description}
+          onChange={(e) => setNewDesc(e.target.value)}
+          onInput={(e) => {
+            e.currentTarget.style.height = "";
+            e.currentTarget.style.height = `${
+              e.currentTarget.scrollHeight + 10
+            }px`;
+          }}
+          onFocus={(e) => {
+            e.currentTarget.style.height = "";
+            e.currentTarget.style.height = `${
+              e.currentTarget.scrollHeight + 10
+            }px`;
+          }}
+        />
+      ) : (
+        <div className="prose prose-black prose-hr:my-2 prose-headings:mt-6 prose-headings:mb-2 prose-p:my-0">
+          <Markdown>{task.description}</Markdown>
+        </div>
+      )}
       <h2 className="text-2xl font-bold">Statistics</h2>
+      <hr className="border-gray-500" />
       You started this task at {oldestChange.timestamp.toLocaleString()}
       <div className="-my-1" />
       {task.status === TaskStatus.CURRENT
@@ -121,6 +165,7 @@ export default function Task() {
       <div className="-my-1" />
       On average, that's {timePerWeekPretty} per week
       <h2 className="text-2xl font-bold">Pick Color</h2>
+      <hr className="border-gray-500" />
       <HexColorPicker
         className="task-color-picker"
         color={color}
@@ -128,6 +173,7 @@ export default function Task() {
       />
       <div className="h-2"></div>
       <h2 className="text-2xl font-bold">Task Priority</h2>
+      <hr className="border-gray-500" />
       <Textbox
         placeholder="Priority"
         value={task.priority.toString()}
